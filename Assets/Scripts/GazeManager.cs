c9949ce007b38e2b44fe885f08410eb5f114f134
Jessie Lyu <jiexinlyu@Jessies-MacBook-Pro-2.local>
2020-04-19 00:03:38 -0700

    initial commit

---
+++ Assets/Scripts/GazeManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeManager : MonoBehaviour
+{
+    public Camera viewCamera;
+    private GameObject lastGazedUpon;
+
+    // Start is called before the first frame update
+    void Start() {}
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckGaze();
+    }
+
+    private void CheckGaze()
+    {
+        if (lastGazedUpon)
+        {
+            lastGazedUpon.SendMessage("NotGazingUpon", SendMessageOptions.DontRequireReceiver);
+        }
+
+        Ray gazeRay = new Ray(viewCamera.transform.position, viewCamera.transform.rotation * Vector3.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(gazeRay, out hit, Mathf.Infinity))
+        {
+            hit.transform.SendMessage("GazingUpon", SendMessageOptions.DontRequireReceiver);
+            lastGazedUpon = hit.transform.gameObject;
+        }
+    }
+}
---
+++ Assets/Scripts/TestObject.cs
@@ -0,0 +1,32 @@
+﻿  
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObject : MonoBehaviour, iGazeReceiver
+{
+    private bool isGazingUpon;
+
+    // Start is called before the first frame update
+    void Start() {}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGazingUpon)
+        {
+            // Do anything you want here, we'll rotate for this demo
+            transform.Rotate(0, 3, 0);
+        }
+    }
+
+    public void GazingUpon()
+    {
+        isGazingUpon = true;
+    }
+
+    public void NotGazingUpon()
+    {
+        isGazingUpon = false;
+    }
+}
