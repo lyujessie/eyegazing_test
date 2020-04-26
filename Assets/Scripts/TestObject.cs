9f4428b3a24a14f9da5952d9d0061e526cead9a6
Jessie Lyu <jiexinlyu@Jessies-MacBook-Pro-2.local>
2020-04-26 15:59:53 -0700

    facial expression change

--- Assets/Scripts/TestObject.cs
+++ Assets/Scripts/TestObject.cs
@@ -6,17 +6,37 @@ using UnityEngine;
 public class TestObject : MonoBehaviour, iGazeReceiver
 {
     private bool isGazingUpon;
+    private bool isRotated;
+    public GameObject face;
+    public Renderer mesh_renderer;
+    public Material smile_face;
+    public Material[] mat;
 
     // Start is called before the first frame update
-    void Start() {}
+    void Start() {
+    	print(gameObject.name);
+    	GameObject mesh = gameObject.transform.Find("Mesh").gameObject;
+    	face = mesh.transform.Find("Face01").gameObject;
+    	mesh_renderer = face.GetComponent<Renderer>();
+    	mat = mesh_renderer.materials;
+    	smile_face = Resources.Load<Material>("Face03");
+    	print(face.name);
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (isGazingUpon)
         {
-            // Do anything you want here, we'll rotate for this demo
-            transform.Rotate(0, 3, 0);
+        	if (!isRotated) {
+        		//transform.Rotate(0, 30, 0);
+        		isRotated = true;
+        		mat[0] = smile_face;
+        		mesh_renderer.materials = mat;
+        		print("changed!");
+        	} else {
+        		print("not rotate!");
+        	}
         }
     }
 
@@ -28,5 +48,6 @@ public class TestObject : MonoBehaviour, iGazeReceiver
     public void NotGazingUpon()
     {
         isGazingUpon = false;
+        isRotated = false;
     }
 }
